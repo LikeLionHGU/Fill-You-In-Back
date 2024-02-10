fe0227c4c487fd62088a1b3573b92b1c342cbc5e
LimYeonKyuu <lim000606@naver.com>
2024-02-10 23:56:08 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/likelion/fillyouinback/FillYouInBackApplication.java
@@ -0,0 +1,15 @@
+package com.likelion.fillyouinback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class FillYouInBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FillYouInBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/likelion/fillyouinback/FillYouInBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.fillyouinback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FillYouInBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
