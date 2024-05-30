44b250134e3ac95da30adc8b465df318b5ecefc4
LimYeonKyuu <lim000606@naver.com>
2024-05-30 17:08:18 +0900

    refactor: @AuthenticationPrincipal을 통해 유저 아이디 확인하도록 수정

--- src/main/java/com/likelion/fillyouinback/member/controller/MemberController.java
+++ src/main/java/com/likelion/fillyouinback/member/controller/MemberController.java
@@ -1,14 +1,13 @@
 package com.likelion.fillyouinback.member.controller;
 
-import com.likelion.fillyouinback.auth.util.JwtUtil;
 import com.likelion.fillyouinback.member.controller.request.UpdateMyProfileRequest;
 import com.likelion.fillyouinback.member.controller.response.*;
 import com.likelion.fillyouinback.member.dto.MemberDto;
 import com.likelion.fillyouinback.s3.exception.S3ImageUploadException;
 import com.likelion.fillyouinback.s3.service.S3Service;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import com.likelion.fillyouinback.member.service.MemberService;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,19 +17,13 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
-
-  @Value("$${custom.jwt.secret}")
-  private String SECRET_KEY;
-
   private final MemberService memberService;
 
   private final S3Service s3Service;
   private static final String PROFILE_IMAGE_DIR = "profile";
 
   @GetMapping("/api/fillyouin/my-profile")
-  public ResponseEntity<MyProfileResponse> getMyProfile(
-      @RequestHeader("Authorization") String bearerToken) {
-    Long memberId = JwtUtil.getMemberId(getToken(bearerToken), SECRET_KEY);
+  public ResponseEntity<MyProfileResponse> getMyProfile(@AuthenticationPrincipal Long memberId) {
     MemberDto memberDto = memberService.getMember(memberId);
     memberDto.setProfileImageUrl(
         s3Service.getImageUrl(PROFILE_IMAGE_DIR, memberDto.getProfileImage()));
@@ -47,8 +40,7 @@ public class MemberController {
       @RequestParam(required = false) String skill,
       @RequestParam(required = false) String job,
       @RequestParam(required = false) String field,
-      @RequestHeader("Authorization") String bearerToken) {
-    Long memberId = JwtUtil.getMemberId(getToken(bearerToken), SECRET_KEY);
+      @AuthenticationPrincipal Long memberId) {
     List<MemberDto> memberDtos =
         memberService.getFilteredMembers(memberId, name, department, semester, skill, job, field);
     memberDtos.forEach(
@@ -60,8 +52,7 @@ public class MemberController {
 
   @GetMapping("/api/fillyouin/members/my-simple-profile-card")
   public ResponseEntity<SimpleProfileCardResponse> getMySimpleProfileCard(
-      @RequestHeader("Authorization") String bearerToken) {
-    Long memberId = JwtUtil.getMemberId(getToken(bearerToken), SECRET_KEY);
+      @AuthenticationPrincipal Long memberId) {
     MemberDto memberDto = memberService.getMember(memberId);
     memberDto.setProfileImageUrl(
         s3Service.getImageUrl(PROFILE_IMAGE_DIR, memberDto.getProfileImage()));
@@ -70,8 +61,7 @@ public class MemberController {
 
   @GetMapping("/api/fillyouin/members/scrap-profile-card")
   public ResponseEntity<ScrapProfileCardListResponse> getScrapProfileCard(
-      @RequestHeader("Authorization") String bearerToken) {
-    Long memberId = JwtUtil.getMemberId(getToken(bearerToken), SECRET_KEY);
+      @AuthenticationPrincipal Long memberId) {
     List<MemberDto> memberDtos = memberService.getScrapMembers(memberId);
     memberDtos.forEach(
         memberDto ->
@@ -82,9 +72,8 @@ public class MemberController {
 
   @GetMapping("/api/fillyouin/members/{memberId}/profile")
   public ResponseEntity<MemberProfileResponse> getMemberProfile(
-      @PathVariable Long memberId, @RequestHeader("Authorization") String bearerToken) {
-    MemberDto memberDto =
-        memberService.getMember(JwtUtil.getMemberId(getToken(bearerToken), SECRET_KEY), memberId);
+      @PathVariable Long memberId, @AuthenticationPrincipal Long myId) {
+    MemberDto memberDto = memberService.getMember(myId, memberId);
     memberDto.setProfileImageUrl(
         s3Service.getImageUrl(PROFILE_IMAGE_DIR, memberDto.getProfileImage()));
     return ResponseEntity.ok(MemberProfileResponse.from(memberDto));
@@ -92,25 +81,17 @@ public class MemberController {
 
   @PutMapping("/api/fillyouin/my-profile")
   public void updateMemberProfile(
-      @RequestHeader("Authorization") String bearerToken,
-      @RequestBody UpdateMyProfileRequest request) {
-    memberService.updateMember(
-        JwtUtil.getMemberId(getToken(bearerToken), SECRET_KEY), MemberDto.from(request));
+      @AuthenticationPrincipal Long memberId, @RequestBody UpdateMyProfileRequest request) {
+    memberService.updateMember(memberId, MemberDto.from(request));
   }
 
   @PostMapping("/api/fillyouin/my-profile/profile-image")
   public void uploadProfileImage(
-      @RequestHeader("Authorization") String bearerToken,
-      @RequestParam("image") MultipartFile image)
+      @AuthenticationPrincipal Long memberId, @RequestParam("image") MultipartFile image)
       throws S3ImageUploadException {
     String fileName = s3Service.upload(image, PROFILE_IMAGE_DIR);
     MemberDto memberDto = MemberDto.builder().profileImage(fileName).build();
     memberDto.setProfileImage(fileName);
-    memberService.updateMemberProfileImage(
-        JwtUtil.getMemberId(getToken(bearerToken), SECRET_KEY), memberDto);
-  }
-
-  private static String getToken(String bearerToken) {
-    return bearerToken.split(" ")[1];
+    memberService.updateMemberProfileImage(memberId, memberDto);
   }
 }
--- src/main/java/com/likelion/fillyouinback/scrapMember/controller/ScrapMemberController.java
+++ src/main/java/com/likelion/fillyouinback/scrapMember/controller/ScrapMemberController.java
@@ -1,38 +1,28 @@
 package com.likelion.fillyouinback.scrapMember.controller;
 
-import com.likelion.fillyouinback.auth.util.JwtUtil;
 import com.likelion.fillyouinback.scrapMember.service.ScrapMemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class ScrapMemberController {
 
-  @Value("$${custom.jwt.secret}")
-  private String SECRET_KEY;
-
   private final ScrapMemberService scrapMemberService;
 
   @PostMapping("/api/fillyouin/scrap-member")
   public ResponseEntity<Void> scrapMember(
-      @RequestHeader("Authorization") String bearerToken, @RequestParam Long scrapMemberId) {
-    Long memberId = JwtUtil.getMemberId(getToken(bearerToken), SECRET_KEY);
+      @RequestParam Long scrapMemberId, @AuthenticationPrincipal Long memberId) {
     scrapMemberService.scrapMember(memberId, scrapMemberId);
     return ResponseEntity.ok().build();
   }
 
   @DeleteMapping("/api/fillyouin/scrap-member")
   public ResponseEntity<Void> deleteScrapMember(
-      @RequestHeader("Authorization") String bearerToken, @RequestParam Long scrapMemberId) {
-    Long memberId = JwtUtil.getMemberId(getToken(bearerToken), SECRET_KEY);
+      @AuthenticationPrincipal Long memberId, @RequestParam Long scrapMemberId) {
     scrapMemberService.deleteScrapMember(memberId, scrapMemberId);
     return ResponseEntity.ok().build();
-  }
-
-  private static String getToken(String bearerToken) {
-    return bearerToken.split(" ")[1];
   }
 }
